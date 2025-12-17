6cc0d85ca5c2c0fc9e749c667dbb1c2054f14b79
DrozsdikAdam <drozsdik17@gmail.com>
2025-12-17 11:42:13 +0100

    auth middleware is done

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+
+export interface AuthRequest extends Request {
+    user?: {
+        userId: string
+        role: string
+    }
+}
+
+export const authenticateToken = (req: AuthRequest, res: Response, next: NextFunction) => {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (!token) {
+        return res.status(401).json({ message: 'Hozzáférés megtagadva: nincs token megadva.' })
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET || 'backup_titkos_kulcs_jwt_hez', (err: any, user: any) => {
+        if (err) {
+            return res.status(403).json({ message: 'Érvénytelen token' })
+        }
+        req.user = user
+        next()
+    })
+}
