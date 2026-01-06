2931b6fa795ed223110668899e75cf2cc7fcf923
DrozsdikAdam <drozsdik17@gmail.com>
2026-01-06 10:03:17 +0100

    refactor: remove custom AuthRequest interface and use default Request type; add user property to Express Request interface

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,14 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 import jwt from 'jsonwebtoken'
 
-export interface AuthRequest extends Request {
-    user?: {
-        userId: string
-        role: string
-    }
-}
-
-export const authenticateToken = (req: AuthRequest, res: Response, next: NextFunction) => {
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
 
