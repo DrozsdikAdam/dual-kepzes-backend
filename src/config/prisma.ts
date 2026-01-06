596eeaa0ef728abc6139c69e8792e5ac35049d51
DrozsdikAdam <drozsdik17@gmail.com>
2026-01-06 12:40:04 +0100

    modifying the prisma client

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,5 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+// Globális változó deklarálása, hogy ne vesszen el újrafordításkor (development módban)
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+    globalForPrisma.prisma ||
+    new PrismaClient({
+        log: ['query'], // Opcionális: segít látni, mit csinál
+    });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
