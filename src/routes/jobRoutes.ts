2a28409096b5bf9693b4749d06cc2e60c6434f37
DrozsdikAdam <drozsdik17@gmail.com>
2026-01-07 10:21:10 +0100

    done with the endpoints using the middlewares and validating the data

---
+++ src/routes/jobRoutes.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { createCompany, createPosition } from "../controllers/jobController";
+import { validate } from "../middlewares/validateMiddleware";
+import {
+  CompanyCreateSchema,
+  PositionCreateSchema,
+} from "../schemas/jobSchema";
+import { authenticateToken } from "../middlewares/authMiddleware";
+
+const router = Router();
+
+router.post(
+  "/companies",
+  authenticateToken,
+  validate(CompanyCreateSchema),
+  createCompany
+);
+router.post(
+  "/positions",
+  authenticateToken,
+  validate(PositionCreateSchema),
+  createPosition
+);
