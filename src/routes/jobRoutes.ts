4b35ba50043209005d651a0806ca58fd07b5a191
DrozsdikAdam <drozsdik17@gmail.com>
2026-01-07 12:45:51 +0100

    feat: add update routes for companies and positions in jobRoutes

--- src/routes/jobRoutes.ts
+++ src/routes/jobRoutes.ts
@@ -1,25 +1,41 @@
 import { Router } from "express";
-import { createCompany, createPosition } from "../controllers/jobController";
+import { createCompany, createPosition, updateCompany, updatePosition } from "../controllers/jobController";
 import { validate } from "../middlewares/validateMiddleware";
 import {
-  CompanyCreateSchema,
-  PositionCreateSchema,
+    CompanyCreateSchema,
+    PositionCreateSchema,
+    CompanyUpdateSchema,
+    PositionUpdateSchema,
 } from "../schemas/jobSchema";
 import { authenticateToken } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.post(
-  "/companies",
-  authenticateToken,
-  validate(CompanyCreateSchema),
-  createCompany
+    "/companies",
+    authenticateToken,
+    validate(CompanyCreateSchema),
+    createCompany
 );
 router.post(
-  "/positions",
-  authenticateToken,
-  validate(PositionCreateSchema),
-  createPosition
+    "/positions",
+    authenticateToken,
+    validate(PositionCreateSchema),
+    createPosition
+);
+
+router.put(
+    "/companies/:id",
+    authenticateToken,
+    validate(CompanyUpdateSchema),
+    updateCompany
+);
+
+router.put(
+    "/positions/:id",
+    authenticateToken,
+    validate(PositionUpdateSchema),
+    updatePosition
 );
 
 export default router;
