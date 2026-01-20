4fd0391ed362aceb1ae91b042090a91524ccee49
DrozsdikAdam <drozsdik17@gmail.com>
2026-01-20 11:57:02 +0100

    feat: Initialize Prisma schema with core application models and add temporary address fields to Company and StudentProfile.

---
+++ prisma/migrations/20260120102538_temp_restore_fields/migration.sql
@@ -0,0 +1,9 @@
+-- AlterTable
+ALTER TABLE "Company" ADD COLUMN     "tempAddress" TEXT,
+ADD COLUMN     "tempCity" TEXT,
+ADD COLUMN     "tempZipCode" TEXT;
+
+-- AlterTable
+ALTER TABLE "StudentProfile" ADD COLUMN     "tempAddress" TEXT,
+ADD COLUMN     "tempCity" TEXT,
+ADD COLUMN     "tempZipCode" TEXT;
