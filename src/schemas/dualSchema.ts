9f01acc36a5b9428cec1275ccc37f5b926f7f45e
DrozsdikAdam <drozsdik17@gmail.com>
2026-01-22 13:28:52 +0100

    updated the schema for the process

--- src/schemas/dualSchema.ts
+++ src/schemas/dualSchema.ts
@@ -1,4 +1,3 @@
-
 import { z } from 'zod';
 
 const dualPartnershipBody = z.object({
@@ -7,11 +6,10 @@ const dualPartnershipBody = z.object({
   uniEmployeeId: z.string().uuid().optional(),
   semester: z.string(),
   contractNumber: z.string().optional(),
-  status: z.enum(['ACTIVE', 'FINISHED', 'TERMINATED']).default('ACTIVE'),
+  status: z.enum(['ACTIVE', 'FINISHED', 'TERMINATED', 'PENDING_MENTOR', 'PENDING_UNIVERSITY']).default('PENDING_MENTOR'),
   startDate: z.coerce.date(),
   endDate: z.coerce.date().optional().nullable(),
 });
-
 
 export const DualPartnershipUpdateSchema = z.object({
   body: dualPartnershipBody.partial(),
@@ -20,4 +18,24 @@ export const DualPartnershipUpdateSchema = z.object({
   })
 });
 
+export const AssignMentorSchema = z.object({
+  params: z.object({
+    id: z.string().uuid()
+  }),
+  body: z.object({
+    mentorId: z.string().uuid()
+  })
+});
+
+export const AssignUniversityUserSchema = z.object({
+  params: z.object({
+    id: z.string().uuid()
+  }),
+  body: z.object({
+    uniEmployeeId: z.string().uuid()
+  })
+});
+
 export type DualPartnershipUpdateRequest = z.infer<typeof DualPartnershipUpdateSchema>;
+export type AssignMentorRequest = z.infer<typeof AssignMentorSchema>;
+export type AssignUniversityUserRequest = z.infer<typeof AssignUniversityUserSchema>;
