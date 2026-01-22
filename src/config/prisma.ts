d42b5dd8825edb20ec0bcdb3d36a58e81c3cd853
DrozsdikAdam <drozsdik17@gmail.com>
2026-01-22 12:29:12 +0100

    feat: Implement Prisma client initialization with global soft delete query extension for specified models.

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -7,7 +7,7 @@ const basePrisma = globalForPrisma.prisma || new PrismaClient();
 const softDeleteModels = [
     "User", "StudentProfile", "CompanyEmployee", "Company",
     "Position", "Tag", "Application", "DualPartnership",
-    "Document", "LogbookEntry", "News"
+    "Document", "LogbookEntry", "News", "Notification"
 ];
 
 const prisma = basePrisma.$extends({
