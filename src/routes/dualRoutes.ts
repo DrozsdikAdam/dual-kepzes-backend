19a89a8025fff0b4e5351e88cf50378cdb88ba06
DrozsdikAdam <drozsdik17@gmail.com>
2026-01-26 10:26:09 +0100

    adding the routes for the new methods

--- src/routes/dualRoutes.ts
+++ src/routes/dualRoutes.ts
@@ -7,7 +7,10 @@ import {
     updatePartnership,
     terminatePartnership,
     assignMentor,
-    assignUniversityUser
+    assignUniversityUser,
+    getStudentPartnerships,
+    getCompanyPartnerships,
+    getUniversityPartnerships
 } from "../controllers/dualController";
 import { validate } from "../middlewares/validateMiddleware";
 import {
@@ -15,13 +18,36 @@ import {
     AssignUniversityUserSchema,
     DualPartnershipUpdateSchema,
 } from "../schemas/dualSchema";
-import { authenticateToken } from "../middlewares/authMiddleware";
+import {
+    authenticateToken,
+    isCompanyEmployee,
+    isStudent,
+    isUniversityStaff
+} from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.get("/",
     authenticateToken,
     getAllPartnerships
+);
+
+router.get("/student",
+    authenticateToken,
+    isStudent,
+    getStudentPartnerships
+);
+
+router.get("/company",
+    authenticateToken,
+    isCompanyEmployee,
+    getCompanyPartnerships
+);
+
+router.get("/university",
+    authenticateToken,
+    isUniversityStaff,
+    getUniversityPartnerships
 );
 
 router.get("/:id",
