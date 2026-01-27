cc41374fb4f68c5cf38a588179948832f7d4d81c
DrozsdikAdam <drozsdik17@gmail.com>
2026-01-27 12:12:49 +0100

    feat: add Redis connection configuration for BullMQ using environment variables

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,7 +1,15 @@
 import { ConnectionOptions } from "bullmq";
 
-export const redisConfig: ConnectionOptions = {
-     host: process.env.REDIS_HOST,
-     port: Number(process.env.REDIS_PORT) || 6379,
-     //password: process.env.REDIS_PASSWORD,
-}
+export const redisConfig: ConnectionOptions = process.env.REDIS_URL
+     ? {
+          host: new URL(process.env.REDIS_URL).hostname,
+          port: Number(new URL(process.env.REDIS_URL).port),
+          //password: new URL(process.env.REDIS_URL).password,
+          //username: new URL(process.env.REDIS_URL).username,
+          tls: process.env.REDIS_URL.includes("rediss://") ? {} : undefined
+     }
+     : {
+          host: process.env.REDIS_HOST || '127.0.0.1',
+          port: Number(process.env.REDIS_PORT) || 6379,
+          //password: process.env.REDIS_PASSWORD,
+     };
