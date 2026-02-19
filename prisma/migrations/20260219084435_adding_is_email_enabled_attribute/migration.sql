36685b1343f6a626b758a4300df4b8485f252ab9
DrozsdikAdam <drozsdik17@gmail.com>
2026-02-19 09:49:57 +0100

    adding new email attribute

---
+++ prisma/migrations/20260219084435_adding_is_email_enabled_attribute/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "isEmailEnabled" BOOLEAN NOT NULL DEFAULT false;
---
+++ prisma/migrations/20260219084941_changeing_is_email_enabled_attribute/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "isEmailEnabled" SET DEFAULT true;
