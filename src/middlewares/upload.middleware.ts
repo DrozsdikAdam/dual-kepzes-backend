6073e480c46fae0199cf60dceb3f06ad0f19429a
DrozsdikAdam <drozsdik17@gmail.com>
2026-03-21 15:16:55 +0100

    adding new utility middleware and new package for more efficient picture handling

---
+++ src/middlewares/upload.middleware.ts
@@ -0,0 +1,24 @@
+import multer from 'multer';
+import { BadRequestError } from '../errors/AppError';
+
+// Memóriában tartjuk a képet, amíg a 'sharp' feldolgozza
+const storage = multer.memoryStorage();
+
+const fileFilter = (req: any, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
+  const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/webp'];
+
+  if (allowedMimeTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    // Ha nem megfelelő a típus, dobjon hibát
+    cb(new BadRequestError('Csak képfájlok (.jpg, .jpeg, .png, .webp) feltöltése engedélyezett!'));
+  }
+};
+
+export const uploadImageMiddleware = multer({
+  storage,
+  limits: {
+    fileSize: 10 * 1024 * 1024, // Maximum 10 MB per fájl feltöltéskor
+  },
+  fileFilter,
+});
